8b9c97262ac42686d765b00625000f9082dcef6e
Ilya Breitburg <ilya.breytburg@gmail.com>
2023-02-19 03:11:16 +0400

    Add icon

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,16 @@ import 'package:forstall/screens/springboard.dart';
 import 'package:forstall/widgets/status_bar/status_bar.dart';
 
 void main() {
-  runApp(const ForstallApp());
+  runApp(
+    Center(
+      child: Container(
+        clipBehavior: Clip.antiAlias,
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(5)),
+        constraints: const BoxConstraints.tightFor(width: 320, height: 568),
+        child: const ForstallApp(),
+      ),
+    ),
+  );
 }
 
 class ForstallApp extends StatelessWidget {
@@ -13,39 +22,34 @@ class ForstallApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        constraints: const BoxConstraints.tightFor(width: 320, height: 568),
-        child: WidgetsApp(
-          pageRouteBuilder: <T>(RouteSettings settings, WidgetBuilder builder) {
-            return ForstallPageRoute<T>(settings: settings, builder: builder);
-          },
-          builder: (BuildContext context, Widget? child) {
-            if (child == null) {
-              return const SizedBox.shrink();
-            }
+    return WidgetsApp(
+      pageRouteBuilder: <T>(RouteSettings settings, WidgetBuilder builder) {
+        return ForstallPageRoute<T>(settings: settings, builder: builder);
+      },
+      builder: (BuildContext context, Widget? child) {
+        if (child == null) {
+          return const SizedBox.shrink();
+        }
 
-            const statusBar = StatusBar.translucent();
+        const statusBar = StatusBar.translucent();
 
-            return Stack(
-              children: [
-                MediaQuery(
-                  data: MediaQuery.of(context).copyWith(
-                    padding: EdgeInsets.only(
-                      top: statusBar.preferredSize.height,
-                    ), // Status Bar Height
-                  ),
-                  child: child,
-                ),
-                statusBar,
-              ],
-            );
-          },
-          debugShowCheckedModeBanner: false,
-          color: const Color.fromRGBO(0, 0, 0, 1),
-          home: const Springboard(),
-        ),
-      ),
+        return Stack(
+          children: [
+            MediaQuery(
+              data: MediaQuery.of(context).copyWith(
+                padding: EdgeInsets.only(
+                  top: statusBar.preferredSize.height,
+                ), // Status Bar Height
+              ),
+              child: child,
+            ),
+            statusBar,
+          ],
+        );
+      },
+      debugShowCheckedModeBanner: false,
+      color: const Color.fromRGBO(0, 0, 0, 1),
+      home: const Springboard(),
     );
   }
 }
